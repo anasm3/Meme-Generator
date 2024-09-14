2a46bd0ae9d4c568aac9ae7ce2561872875a44d6
anasm3 <anasmukhtar08@gmail.com>
2024-09-14 23:46:29 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from "react"
+import Header from "./components/Header";
+import Meme from "./components/meme";
+
+
+const App = () => {
+  return (
+      <div>
+          <Header />
+          <Meme />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Meme.jsx
@@ -0,0 +1,35 @@
+// import React  from "react";
+
+const Meme = () => {
+    return (
+        <main>
+            <form className="form">
+                <div>
+                    <label>Top Text
+                        <input
+                            type="text"
+                            placeholder="Shut up"
+                            className="form--input"
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>Bottom Text
+                    <input
+                            type="text"
+                            placeholder="and take my money"
+                            className="form--input"
+                        />
+                    </label>
+                </div>
+                <button
+                    className="form--button"
+                >
+                Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+    )
+}
+
+export default Meme
